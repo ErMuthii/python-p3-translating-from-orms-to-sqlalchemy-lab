61ca51ac96e5a846143b661176acf8542c42d6c2
ErMuthii <er.muthii@gmail.com>
2024-02-15 00:20:50 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,35 @@
 from models import Dog
 
+
 def create_table(base):
-    pass
+    if __name__ == '__main__':
+        engine = create_engine('sqlite:///:memory:')
+        base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+
 
 def get_all(session):
-    pass
+    dogs = session.query(Dog).all()
+    print (dogs)
 
 def find_by_name(session, name):
-    pass
+    dog = session.query(Dog).filter(Dog.name == name).first()
+    print(dog)
+  
 
 def find_by_id(session, id):
-    pass
+    dog = session.query(Dog).filter(Dog.id == id).first()
+    print(dog)
+
+  
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+     dog = session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
+     print(dog)
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
